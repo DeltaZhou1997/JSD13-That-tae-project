29d2c4d8505d62cf3c59afe8193d9faf3b4014d9
kydenSp <supisa.wo@gmail.com>
2026-09-17 13:19:56 +0700

    update cart

--- Clients/src/components/Cream/Cart.jsx
+++ Clients/src/components/Cream/Cart.jsx
@@ -12,7 +12,7 @@ export default function Cart() {
   const total = subtotal + shippingFee;
 
   useEffect(() => {
-    if (subtotal >= 1499) {
+    if (subtotal >= 1499 && !hasAlerted ) {
       alert("🎉ยอดสั่งซื้อของคุณถึง 1,499 บาท รับแต้มสะสม!");
       setHasAlerted(true);
       } else if (subtotal < 1499) {
